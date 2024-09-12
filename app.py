8111f8deb35739912b88fc76d8c3c6dd34d7b27f
SanjayS059 <sanjaysrinivas059@gmail.com>
2024-09-12 15:37:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from google.colab import files
+files.download('/content/drive/MyDrive/Colab Notebooks/model.py')
+
+!ls
+
+
+import streamlit as st
+from model import role_mapping_df,vectorize_role_descriptions, recommend_sap_role
+
+
+role_descriptions = role_mapping_df['mcDisplayName'].values
+sap_roles = role_mapping_df['mcMSKEYVALUE'].values
+mskeys = role_mapping_df['mcMSKEY'].values
+
+vectorizer, tfidf_matrix = vectorize_role_descriptions(role_descriptions)
+
+st.title("AI-Powered SAP Role Recommendation System")
+role_description_input = st.text_area("Enter a role description:")
+
+if st.button("Generate Role"):
+    mskey, sap_role, display_name = recommend_sap_role(role_description_input, vectorizer, tfidf_matrix, mskeys, sap_roles, role_descriptions)
+    st.write(f"Recommended SAP Role: {sap_role}")
+    st.write(f"Role Display Name: {display_name}")
+    st.write(f"Role MSKEY: {mskey}")
