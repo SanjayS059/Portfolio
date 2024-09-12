97981254b2d135ecc4e5d197a11704dc0ddb3001
SanjayS059 <sanjaysrinivas059@gmail.com>
2024-09-12 14:51:14 +0530

    Add files via upload

---
+++ Gen AI Projects/Dynamic Role Mapping/model.py
@@ -0,0 +1,34 @@
+pip install streamlit
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def load_role_mapping_data():
+
+    file_path = '/content/Book1(Sheet1).csv'
+    role_mapping_df = pd.read_csv(file_path)
+    return role_mapping_df
+
+role_mapping_df = load_role_mapping_data()
+
+role_descriptions = role_mapping_df['mcDisplayName'].values
+sap_roles = role_mapping_df['mcMSKEYVALUE'].values
+mskeys = role_mapping_df['mcMSKEY'].values
+
+def vectorize_role_descriptions(descriptions):
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(descriptions)
+    return vectorizer, tfidf_matrix
+
+vectorizer, tfidf_matrix = vectorize_role_descriptions(role_descriptions)
+
+def recommend_sap_role(input_description, vectorizer, tfidf_matrix, mskeys, sap_roles, descriptions):
+
+    input_tfidf = vectorizer.transform([input_description])
+    cosine_similarities = cosine_similarity(input_tfidf, tfidf_matrix).flatten()
+    best_match_index = cosine_similarities.argmax()
+    return mskeys[best_match_index], sap_roles[best_match_index], descriptions[best_match_index]
+
+
